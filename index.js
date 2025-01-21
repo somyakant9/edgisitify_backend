0bc9669120e1b6a8e93ec59cdf5450c98b73f10d
somyakant9 <msomya143@gmail.com>
2025-01-21 12:53:27 +0530

    added port

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mongoose.connect(DB).then(() => {
 });
 
 const port = process.env.PORT || 3000;
-
-const server = app.listen(port, () => {
+// console.log(port);
+const server = app.listen(port, '0.0.0.0' ,() => {
   console.log(`App running on port ${port}...`);
 });
